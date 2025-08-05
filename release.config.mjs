a4dde4a3c0d1faafcf3499d4ee419babfabea13c
François Lefebvre <francois.lfe@gmail.com>
2025-08-05 08:05:56 +0200

    fix(release): asset names

--- release.config.mjs
+++ release.config.mjs
@@ -26,22 +26,22 @@ export default {
           {
             path: 'binaries/enocean-cli-alpine-arm64',
             label: 'Alpine ARM64',
-            name: 'enocean',
+            name: 'enocean-alpine',
           },
           {
             path: 'binaries/enocean-cli-linux-x64',
             label: 'Linux x64',
-            name: 'enocean',
+            name: 'enocean-linux',
           },
           {
             path: 'binaries/enocean-cli-macos-arm64',
             label: 'MacOS (M chips)',
-            name: 'enocean',
+            name: 'enocean-macos',
           },
           {
             path: 'binaries/enocean-cli-win-x64.exe',
             label: 'Windows x64',
-            name: 'enocean',
+            name: 'enocean.exe',
           },
         ],
       },
