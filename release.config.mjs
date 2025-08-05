3671fcbbac8a1e6ff5993b024cf15d5c146dc333
François Lefebvre <francois.lfe@gmail.com>
2025-08-05 07:52:58 +0200

    feat(release): define assets labels

--- release.config.mjs
+++ release.config.mjs
@@ -24,8 +24,24 @@ export default {
       {
         assets: [
           {
-            path: 'binaries/*',
-            label: 'Binaries',
+            path: 'binaries/enocean-cli-alpine-arm64',
+            label: 'Alpine ARM64',
+            name: 'enocean',
+          },
+          {
+            path: 'binaries/enocean-cli-linux-x64',
+            label: 'Linux x64',
+            name: 'enocean',
+          },
+          {
+            path: 'binaries/enocean-cli-macos-arm64',
+            label: 'MacOS (M chips)',
+            name: 'enocean',
+          },
+          {
+            path: 'binaries/enocean-cli-win-x64',
+            label: 'Windows x64',
+            name: 'enocean',
           },
         ],
       },
